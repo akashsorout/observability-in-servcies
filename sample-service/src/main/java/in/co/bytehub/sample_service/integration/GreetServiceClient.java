87003d990af32ab0fe3a4d41029370cc4d86abd3
akashchaudhary <akashsorout@gmail.com>
2025-07-13 15:45:19 +0530

    add logging in greet service client

--- sample-service/src/main/java/in/co/bytehub/sample_service/integration/GreetServiceClient.java
+++ sample-service/src/main/java/in/co/bytehub/sample_service/integration/GreetServiceClient.java
@@ -1,5 +1,7 @@
 package in.co.bytehub.sample_service.integration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -10,6 +12,7 @@ import java.net.http.HttpResponse;
 @Component
 public class GreetServiceClient {
 
+    private static final Logger logger = LoggerFactory.getLogger(GreetServiceClient.class);
     private static final String GREET_SERVICE_URL = "http://greet-service:8080%s";
 
     private final HttpClient httpClient;
@@ -33,15 +36,20 @@ public class GreetServiceClient {
 
     private String callServiceAndHandleResponse(HttpRequest httpRequest) {
         try {
+            logger.info("Calling greet service with request: {}", httpRequest.uri());
             var response = this.httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
             if (response == null) {
+                logger.error("Response from greet service is null");
                 throw new RuntimeException("Response is null");
             }
             if (response.statusCode() / 100 != 2) {
+                logger.error("Response from greet service is not successful: {}", response.statusCode());
+
                 throw new RuntimeException("Response is not successful: %s".formatted(response.statusCode()));
             }
             return response.body();
         } catch (Exception ex) {
+            logger.error("Failed to call greet service", ex);
             throw new RuntimeException("Failed to call greet service", ex);
         }
 
